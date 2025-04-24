00f15821c71c34b7c529d00d1a3d3687a3e8f38d
burru <burru@LAPTOP-6S6ME6G9>
2025-04-24 18:31:04 +0200

    static

--- Vecinapp/Vecinapp/settings.py
+++ Vecinapp/Vecinapp/settings.py
@@ -124,8 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "Web/static"),          # si usas Web/static/Vecinapp/css
+    os.path.join(BASE_DIR, "Comunidad/static")     # si usas Comunidad/static/comunidad/css
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
