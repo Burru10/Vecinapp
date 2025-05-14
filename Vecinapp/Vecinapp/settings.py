8e167f9df156e02a21b7e92f3f46824bcc56bf6d
burru <burru@LAPTOP-6S6ME6G9>
2025-05-14 13:20:02 +0200

    Cambios

--- Vecinapp/Vecinapp/settings.py
+++ Vecinapp/Vecinapp/settings.py
@@ -81,20 +81,12 @@ WSGI_APPLICATION = 'Vecinapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if os.environ.get('DJANGO_ENV') == 'production':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': '/mnt/data/db.sqlite3',  # Usamos /mnt/data para producción
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',  # Usamos el archivo local db.sqlite3 en desarrollo
-        }
-    }
+}
 
 
 # Password validation
